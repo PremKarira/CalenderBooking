b7ef521eb54a2a5b92b2feceec43c9240743d18d
Prem Karira <karira46@gmail.com>
2022-06-02 19:37:36 +0530

    gh-page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,113 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import resourceTimeGridPlugin from "@fullcalendar/resource-timegrid";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+        calendarComponentRef = React.createRef();
+        state = {
+            events: [
+                { id: 1, title: "event 1", date: "2022-06-01" },
+                {
+                    title: "event 2",
+                    start: "2022-06-01",
+                    end: "2022-06-05",
+                    allDay: true,
+                    HostName: "William"
+                },
+                {
+                    title: "event 3",
+                    start: "2022-06-05",
+                    end: "2022-06-07",
+                    allDay: true
+                },
+                {
+                    title: "event 4",
+                    start: "2022-06-05",
+                    end: "2022-06-07",
+                    allDay: true
+                },
+                {
+                    title: "event 5",
+                    start: "2022-06-05",
+                    end: "2022-06-07",
+                    allDay: true
+                },
+                {
+                    title: "event 6",
+                    start: "2022-06-05",
+                    end: "2022-06-07",
+                    allDay: true
+                }
+            ]
+        };
+
+        handleDateClick = arg => {
+            alert(arg.dateStr);
+        };
+
+        handleSelectedDates = info => {
+            alert("selected " + info.startStr + " to " + info.endStr);
+            const title = prompt("What's the name of the title");
+            console.log(info);
+            if (title != null) {
+                const newEvent = {
+                    title,
+                    start: info.startStr,
+                    end: info.endStr
+                };
+                const data = [...this.state.events, newEvent];
+                this.setState({ events: data });
+                console.log("here", data);
+            } else {
+                console.log("nothing");
+            }
+        };
+
+        render() {
+            return ( < div >
+                <
+
+                FullCalendar schedulerLicenseKey = "GPL-My-Project-Is-Open-Source"
+                ref = { this.calendarComponentRef }
+                defaultView = "dayGridMonth"
+                dateClick = { this.handleDateClick }
+                displayEventTime = { true }
+                header = {
+                    {
+                        left: "prev,next today",
+                        center: "title",
+                        right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek"
+                    }
+                }
+                selectable = { true }
+                plugins = {
+                    [
+                        dayGridPlugin,
+                        interactionPlugin,
+                        timeGridPlugin,
+                        resourceTimeGridPlugin
+                    ]
+                }
+                eventClick = {
+                    event => {
+                        console.log(event.event._def.publicId);
+                    }
+                }
+                events = { this.state.events }
+                select = { this.handleSelectedDates }
+                eventLimit = { 3 }
+                /> </div >
+            );
+        }
+    }
+    // const cal = document.querySelector('#cal')
+    // cal.addEventListener('click', _ => {
+
+// })
+const rootElement = document.getElementById("root");
+ReactDOM.render( < App / > , rootElement);
